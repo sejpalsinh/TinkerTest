9d7b3b5ea3c6a2bb6598131e32d2f1c3d6c46d84
Sejpalsinh Jadeja <sjadeja137@rku.ac.in>
2021-06-13 16:52:36 +0530

    tale

--- tinkerdemo.py
+++ tinkerdemo.py
@@ -1,11 +1,26 @@
 from tkinter import *
-from tkintercomponents import Table
+from tkintercomponents import *
 tk_Root = Tk()
 
+
 # Gui Logic
-tk_Root.minsize(1000, 600)
-tk_Root.maxsize(1000, 600)
 headers = ["Name","Age","City"]
 datalst = [("Sejpal",18,"Rajkot"),("Sejpal",18,"Rajkot"),("Sejpal",18,"Rajkot"),("Sejpal",18,"Rajkot"),("Sejpal",18,"Rajkot")]
-tbl = Table(tk_Root,datalst,headers)
+def checkboxtable():
+    coldDataId = []
+    total_columns = len(datalst[0])
+    for colId in range(1, total_columns + 1):
+        coldDataId.append("#" + str(colId))
+    s = ttk.Treeview(columns=tuple(coldDataId), selectmode='browse')
+    s.heading('#0', text='Select')
+    headerCounter = 0
+    for header in coldDataId:
+        print(header)
+        s.heading(header, text=headers[headerCounter])
+        headerCounter = headerCounter + 1
+    s.pack(side="left")
+    v = PhotoImage(file='images/uncheck.png')
+    for data in datalst:
+        s.insert('', 'end', values=tuple(data), image=v)
+checkboxtable()
 tk_Root.mainloop()
--- tkintercomponents.py
+++ tkintercomponents.py
@@ -1,4 +1,8 @@
 from tkinter import ttk
+from tkinter import *
+import PIL
+from PIL import ImageTk, Image
+
 
 class Table:
     def __init__(self, root, datalst, headinglst, tableside='right', tableheading = "headings", colWidth=90):
@@ -13,11 +17,26 @@ class Table:
         print(treev["columns"])
         treev['show'] = tableheading
         for col in range(total_columns):
-            print(col)
             treev.column(col, width=colWidth, anchor='c')
             treev.heading(col, text=headinglst[col])
         for row in range(total_rows):
             treev.insert("", 'end', values=datalst[row])
 
 
-
+class CheckboxTable:
+    def checkboxtable(datalst,headers):
+        coldDataId = []
+        total_columns = len(datalst[0])
+        for colId in range(1, total_columns + 1):
+            coldDataId.append("#" + str(colId))
+        s = ttk.Treeview(columns=tuple(coldDataId),selectmode='browse')
+        s.heading('#0', text='Select')
+        headerCounter = 0
+        for header in coldDataId:
+            print(header)
+            s.heading(header, text=headers[headerCounter])
+            headerCounter = headerCounter + 1
+        s.pack(side="left")
+        v = PhotoImage(file='images/uncheck.png')
+        for data in datalst:
+            s.insert('', 'end', values=tuple(data), image=v)
